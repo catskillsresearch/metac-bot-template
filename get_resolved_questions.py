78903b848bb5e68a8796259987a88271c4ba4b06
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-05-02 23:05:32 -0400

    foo

---
+++ get_resolved_questions.py
@@ -0,0 +1,54 @@
+from forecasting_tools import MetaculusApi, ApiFilter
+import asyncio
+
+def get_resolved_questions(tournament_id):
+    resolved_questions = []
+    offset = 0
+    batch_size = 100
+    while True:
+        api_filter = ApiFilter(
+                    allowed_tournaments=[tournament_id],
+                    allowed_statuses=["resolved"],
+                    limit=batch_size,
+                    offset=offset
+                )
+        batch = asyncio.run(MetaculusApi.get_questions_matching_filter(api_filter))
+        if not batch:
+            break
+        resolved_questions.extend(batch)
+        if len(batch) < batch_size:
+            break
+        offset += batch_size
+        if offset > 300:
+            break
+        print("offset", offset)
+    
+    return resolved_questions
+
+if __name__=="__main__":
+
+    ## Secrets
+    from omegaconf import OmegaConf
+    config = OmegaConf.load("../tokens.yaml")
+    
+    import os
+    for key in config:
+        os.environ[key] = config[key]
+    
+    ## Retrieve all old resolved questions
+    tourneys=[MetaculusApi.AI_COMPETITION_ID_Q3, 
+              MetaculusApi.AI_COMPETITION_ID_Q4, 
+              MetaculusApi.AI_COMPETITION_ID_Q1, 
+              MetaculusApi.AI_COMPETITION_ID_Q2]   
+    questions = []
+    for tournament_id in tourneys:
+        print(tournament_id)
+        resolved_questions = get_resolved_questions(tournament_id)
+        questions.extend(resolved_questions)
+    print("retrieved", len(questions))
+
+    from pickle_objects import *
+    qfn = 'all_questions.pkl'
+    pickle_objects(questions, qfn)
+    q2 = unpickle_objects(qfn)
+    print("saved", len(q2))
