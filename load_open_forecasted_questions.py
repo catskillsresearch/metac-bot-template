44b3cab0129a55ab45232ccca059a6a53d1dfbaa
Lars Ericson <lars.ericson@catskillsresearch.com>
2025-05-12 16:52:32 -0400

    cleanup

--- load_open_forecasted_questions.py
+++ load_open_forecasted_questions.py
@@ -1,13 +1,13 @@
+from forecasting_tools import MetaculusApi, ApiFilter
+from datetime import datetime, timedelta
+import asyncio
+
 def load_open_forecasted_questions(num_questions = 1):
-    from forecasting_tools import MetaculusApi, ApiFilter
-    from datetime import datetime, timedelta
     api_filter = ApiFilter(
     allowed_statuses=["open"],
     num_forecasters_gte=40,
     scheduled_resolve_time_lt=datetime.now() + timedelta(days=365),
     includes_bots_in_aggregates=False )
-
-    import asyncio
     if num_questions:
         questions = asyncio.run(MetaculusApi.get_questions_matching_filter(
             api_filter, num_questions=num_questions, randomly_sample=True))
@@ -20,7 +20,8 @@ def load_open_forecasted_questions(num_questions = 1):
 if __name__=="__main__":
     from load_secrets import load_secrets
     load_secrets()
-    questions = load_open_forecasted_questions(4)
+    questions = load_open_forecasted_questions(1)
     for question in questions:
         print(type(question))
+        print(vars(question).keys())
 
--- load_questions.py
+++ load_questions.py
@@ -6,7 +6,7 @@ def load_questions(num_questions = 4, perennial = False):
         questions = load_perennial_questions()
     else:
         from load_open_forecasted_questions import load_open_forecasted_questions
-        questions = load_open_forecasted_questions()
+        questions = load_open_forecasted_questions(num_questions)
 
     from community_forecast import community_forecast
     id_to_forecast = {question.api_json['id']: community_forecast(question) for question in questions}
@@ -14,10 +14,12 @@ def load_questions(num_questions = 4, perennial = False):
     from flatten_dict import flatten_dict
     from datetime import datetime, timedelta
     df = pd.DataFrame([flatten_dict(q.api_json, sep='_') for q in questions])
+    df['id_of_post'] = [q.id_of_post for q in questions]
+    df['id_of_question'] = [q.id_of_question for q in questions]
     df['question_options'] = df['question_options'].apply(repr)
     df['today'] = datetime.now().strftime("%Y-%m-%d")
     df['crowd'] = df.apply(lambda row: id_to_forecast[row.id], axis=1)
-    df = df[['id', 'today', 'open_time', 'scheduled_resolve_time', 'title', 'question_description',
+    df = df[['id_of_question', 'id_of_post', 'today', 'open_time', 'scheduled_resolve_time', 'title', 'question_description',
             'question_resolution_criteria', 'question_fine_print', 'question_type',
             'question_options', 'question_group_variable', 'question_question_weight',
             'question_unit', 'question_open_upper_bound', 'question_open_lower_bound',
@@ -32,8 +34,12 @@ if __name__=="__main__":
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
     print('LIVE ANY')
-    questions, df = load_questions()
-    print(df)
-    print('PERENNIAL')
+    questions, df = load_questions(40)
+    print(df[['id_of_question', 'id_of_post']])
+    for question in questions:
+        print(question.id_of_question, question.id_of_post)
+    print('\nPERENNIAL')
     questions, df = load_questions(perennial=True)
-    print(df)
+    print(df[['id_of_question', 'id_of_post']])
+    for question in questions:
+        print(question.id_of_question, question.id_of_post)
